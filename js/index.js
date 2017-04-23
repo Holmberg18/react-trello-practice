ec126a284f4d0c8d2c09798d21eaae3b19adc55a
Jon Holmberg <Jon Holmberg>
2017-04-23 00:07:33 -0700

    list-container practice

--- js/index.js
+++ js/index.js
@@ -1,2 +1,11 @@
-require('babel-polyfill');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+import Card from './components/card';
+import List from './components/list';
+import Board from './components/board';
+import ListContainer from './components/list-container';
+var listsArr = ["list1","list2","list3"];
+document.addEventListener('DOMContentLoaded', () =>
+    ReactDOM.render(<ListContainer />, document.getElementById('app'))
+);
